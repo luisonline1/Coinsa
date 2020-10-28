dd2caec9a2117c2b6c73ea46683e901dcabedf19
Luis River <luisonline1@gmail.com>
2020-10-27 23:33:10 -0600

    Primeros avances login

---
+++ Api/Api/Controllers/CoinsaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    public class CoinsaController : Controller
+    {
+        [HttpGet]
+        public string Login(string usuario, string Contraseña)
+        {
+            try
+            {
+                using (BdCoinsa BdCoinsa = new BdCoinsa())
+                {
+                    
+                }
+                return "";
+            }
+            catch (Exception Ex)
+            {
+                return Ex.Message;
+            }
+        }
+    }
+}
